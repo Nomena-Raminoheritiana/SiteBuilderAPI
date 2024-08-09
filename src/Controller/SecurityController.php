36e500219d6b1e1f5ab35ee770d48b4829931c2a
Raminoheritiana Nomena <raminoheritiana.nr@pg.com>
2024-08-09 16:45:44 +0300

    [dev] new verifyToken endpoint created

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Services\Token\JWTService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'app_security_login')]
     public function login(AuthenticationUtils $authenticationUtils): JsonResponse
     {
         $user = $this->getUser();
@@ -22,5 +25,21 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->json(['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/api/verifyToken', name: 'app_security_verifytoken', methods: ['POST'])]
+    public function verifyToken(Request $request, JWTService $JWTService): JsonResponse
+    {
+        if($request->getPayload()->has('token')) {
+            $token = $JWTService->parseToken($request->getPayload()->get('token'));
+            if($JWTService->verifyToken($token)) {
+                return $this->json([
+                    'verified' => true
+                ]);
+            }
+        }
+        return $this->json([
+           'verified' => false
+        ], Response::HTTP_UNAUTHORIZED);
     }
 }
