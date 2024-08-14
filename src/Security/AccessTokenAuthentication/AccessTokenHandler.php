4578b716d7d96a494301b69c11dc0c1d869d2a82
Nomena Raminoheritiana <n.raminoheritiana@gmail.com>
2024-08-14 16:10:22 +0300

    [dev][security][api] delete user Token when token is not valid

--- src/Security/AccessTokenAuthentication/AccessTokenHandler.php
+++ src/Security/AccessTokenAuthentication/AccessTokenHandler.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Security;
+namespace App\Security\AccessTokenAuthentication;
 
 use App\Repository\UserRepository;
 use App\Services\Token\JWTService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -12,22 +13,26 @@ class AccessTokenHandler implements AccessTokenHandlerInterface
 {
     public function __construct(
         private UserRepository $userRepository,
-        private JWTService $JWTService
+        private JWTService $JWTService,
+        private EntityManagerInterface $entityManager
     ) {
     }
 
     public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
     {
         $token = $this->JWTService->parseToken($accessToken);
-        if(!$this->JWTService->verifyToken($token)) {
-            throw new BadCredentialsException('Invalid credentials.');
-        }
         $user = $this->userRepository->findOneBy([
             "token" => $accessToken
         ]);
-        if($user === null) {
+        if(!$this->JWTService->verifyToken($token)) {
+            if($user !== null) {
+                $user->setToken(null);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
             throw new BadCredentialsException('Invalid credentials.');
         }
+        $user === null && throw new BadCredentialsException('Invalid credentials.');
 
         return new UserBadge($user->getUserIdentifier());
     }
